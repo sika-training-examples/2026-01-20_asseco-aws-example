fac33ac7edaceee9eb1a4bb518b90e750f7ca553
Ondrej Sika <ondrej@ondrejsika.com>
2026-01-20 14:46:01 +0000

    feat: Disable EKS Auto Mode

--- eks.tf
+++ eks.tf
@@ -7,11 +7,6 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
